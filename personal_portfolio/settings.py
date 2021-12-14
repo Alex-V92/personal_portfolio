70c78f0aaaf4015d3472313e4b899756c5688296
Alex-V92 <60703679+Alex-V92@users.noreply.github.com>
2021-12-14 15:26:12 +0300

    Update settings.py
    del secret key

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j)5%qa^y3@s5r#5s^9p878t$=2ufz!7_=2t1+2=#lr%1am3yi_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
